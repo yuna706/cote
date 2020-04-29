c0f0254e9881bed8ff3d900d7c73e1da97efc64f
yuna706 <riyu6027@naver.com>
2020-04-29 13:25:52 +0900

    십자 뒤집기

--- yuna pro/sip-97.cpp
+++ yuna pro/sip-97.cpp
@@ -9,22 +9,30 @@ int main() {
 	{
 		for (int j = 1; j < 20; j++)
 		{
-			std::cin > a[i][j];
+			std::cin >> a[i][j];
 		}
 	}
 
-	std::cin << n;
+	std::cin >> n;
 
 	for (int i = 1; i <= n; i++)
 	{
-		std::cin << x[i] << y[i];
+		std::cin >> x[i] >> y[i];
 	}
 
-	for (int i = 1; i < 20; i++)
+	for (int i = 1; i <= n; i++)
 	{
+		/* 세로 뒤집기 */
 		for (int j = 0; j < 20; j++)
 		{
-			a[i][j] ? 0 : 1;
+			if(a[j][x[i]] == 1) a[j][x[i]] = 0;
+			else a[j][x[i]] = 1;
+		}
+		/* 가로 뒤집기 */
+		for (int j = 0; j < 20; j++)
+		{
+			if(a[y[i]][j] == 1) a[y[i]][j] = 0;
+			else a[y[i]][j] = 1;
 		}
 	}
 
@@ -32,7 +40,8 @@ int main() {
 	{
 		for (int j = 1; j < 20; j++)
 		{
-			std::cout >> a[i][j];
+			std::cout << a[i][j] <<" ";
 		}
+		std::cout << "\n";
 	}
 }
